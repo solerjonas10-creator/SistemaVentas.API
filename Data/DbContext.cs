3e9c28181787d7a44bea157db676097431ae78e9
Jonas <soler.jonas10@gmail.com>
2026-01-28 16:54:00 -0300

    PrimerConeccionDBOracle

---
+++ Data/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaVentas.API.Data
+{
+    public class VentasContext : DbContext
+    {
+        public VentasContext(DbContextOptions<VentasContext> options) : base(options)
+        {
+        }
+
+        // public DbSet<Producto> Productos { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaVentas.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("OracleDbConnection");
+
+builder.Services.AddDbContext<VentasContext>(options => options.UseOracle(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
