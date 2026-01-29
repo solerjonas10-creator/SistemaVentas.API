190db9254ee4096ca59121c2b01acac49530665a
Jonas <soler.jonas10@gmail.com>
2026-01-28 22:09:30 -0300

    CreateProductoControllerGet

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVentas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult Read(int id)
+        {
+            return Ok(new {id, Nombre = "Jonas"});
+        }
+    }
+}
