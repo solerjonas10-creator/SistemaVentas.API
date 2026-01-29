309083f1748a7266f361c1b51d970ca3a4cb4de1
Jonas <soler.jonas10@gmail.com>
2026-01-28 22:18:58 -0300

    UpdateProgram.cs

--- Program.cs
+++ Program.cs
@@ -1,33 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using SistemaVentas.API.Data;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("OracleDbConnection");
-
 builder.Services.AddDbContext<VentasContext>(options => options.UseOracle(connectionString));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sistema de ventas API v1");
+        c.RoutePrefix = "/swagger";
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sistema de ventas API"));
-
 app.Run();
