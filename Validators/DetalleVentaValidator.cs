676d39849259ab49a90a86528ae4ce3ec7f48e81
Jonas <soler.jonas10@gmail.com>
2026-01-28 21:43:57 -0300

    CreateDetalleVentaValidator

---
+++ Validators/DetalleVentaValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SistemaVentas.API.Models;
+
+namespace SistemaVentas.API.Validators
+{
+    public class DetalleVentaValidator : AbstractValidator<DetalleVenta>
+    {
+        public DetalleVentaValidator()
+        {
+            RuleFor(detalle => detalle.Cantidad)
+                .GreaterThan(0).WithMessage("El campo Cantidad debe ser mayor a 0.");
+
+            RuleFor(detalle => detalle.Precio)
+                .GreaterThan(0).WithMessage("El campo PrecioUnitario debe ser mayor a 0.");
+
+
+        }
+    }
+}
